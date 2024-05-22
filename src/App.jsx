b06c47b1fb13ef6e013f16605883dcf07a747898
Yohannes Mesay <johnnymesay@gmail.com>
2024-05-22 16:31:42 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
