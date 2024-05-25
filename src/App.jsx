63949c6d4e3a3c360698d84e66a0bc617cf21053
Yohannes Mesay <johnnymesay@gmail.com>
2024-05-25 12:04:42 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Resource from "./pages/Resource";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          
           <Route path="/" element={<Home />} />
+          <Route path="/resource" element={<Resource />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
 export default App;
-
